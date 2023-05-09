96a9c338f7743d4b404a0e85649507efc9a8fb9c
Marcin <gasiorek.mail@gmail.com>
2023-05-09 08:58:40 +0200

    more tests

---
+++ src/pl/marcin/BankAccount.java
@@ -0,0 +1,37 @@
+package pl.marcin;
+
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private double balance;
+    public static final int CHECKING = 1;
+    public static final int SAVINGS= 2;
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int typeOfAccount ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+        this.accountType = typeOfAccount;
+    }
+//    branch is true if transaction is in bank with casier
+//    false if in ATM or internet
+    public double deposit(double amount, boolean branch){
+        balance += amount;
+        return balance;
+    }
+    public double withdraw(double amount, boolean branch){
+       if((amount> 500) && !branch){
+           throw new IllegalArgumentException();
+       }
+        balance -= amount;
+        return balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    public boolean isChecking(){
+        return accountType == CHECKING;
+    }
+}
---
+++ src/pl/marcin/BankAccountTest.java
@@ -0,0 +1,72 @@
+package pl.marcin;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    BankAccount account;
+    private static int count;
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("this executes before any test cases...Count = " + count++
+        );
+    }
+    @BeforeEach
+    public void setup(){
+        account = new BankAccount("Marcin","Gasiorek",1000,BankAccount.CHECKING);
+        System.out.println("running a test...");
+    }
+
+
+    @Test
+    void deposit() throws Exception{
+        double balance = account.deposit(200,true);
+        assertEquals(1200,balance,0);
+
+    }
+
+    @Test
+    void withdraw_branch() throws Exception{
+        double balance = account.withdraw(600,true);
+        assertEquals(400,balance,0);
+    }
+    @Test()
+    void withdraw_notbranchThrows() throws Exception{
+        assertThrows(IllegalArgumentException.class,() -> {account.withdraw(600,false);
+        });
+
+    }
+    @Test()
+    void withdraw_notbranch() throws Exception{
+        double balance = account.withdraw(400,false);
+        assertEquals(600,balance,0);
+
+    }
+
+    @Test
+    void getBalance_deposit() throws Exception{
+        account.deposit(200,true);
+        assertEquals(1200.00,account.getBalance(),0);
+    }
+    @Test
+    void getBalance_withdraw() throws Exception{
+        account.withdraw(200,true);
+        assertEquals(800.00,account.getBalance(),0);
+    }
+    @Test
+    public void isChecking_true(){
+        assertTrue(account.isChecking(), "The account is not a checking account");
+    }
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("this executes after all methods are executed count = " + count++);
+    }
+    @AfterEach
+    public void tearDown(){
+        System.out.println("count = " + count++);
+    }
+
+
+}
